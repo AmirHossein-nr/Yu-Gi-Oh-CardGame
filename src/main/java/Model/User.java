c7511420824bc10df38dd60bc853fcf54a291da0
Mostafa <mostafa.hosseini788@gmail.com>
2021-05-03 08:48:50 -0700

    ScoreBoard menu done

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class User {
 
@@ -8,7 +9,7 @@ public class User {
     private String username;
     private String password;
     private String nickName;
-    private static ArrayList<User> allUsers = new ArrayList<User>();
+    private static ArrayList<User> allUsers;
     private long score;
     private long lifePoint;
     private long money;
@@ -54,6 +55,14 @@ public class User {
         return nickName;
     }
 
+    public long getScore() {
+        return score;
+    }
+
+    public static ArrayList<User> getAllUsers() {
+        return allUsers;
+    }
+
     public static User getUserByUsername(String username) {
         for (User user : allUsers) {
             if (user.getUsername().equals(username)) {
@@ -70,6 +79,21 @@ public class User {
             }
         }
         return null;
+    }
+
+    public static void sortAllUsers() {
+        for (int i = allUsers.size() - 2; i >= 0 ; i--) {
+            for (int j = 0; j <= allUsers.size() - 2; j++) {
+                if (allUsers.get(j).score > allUsers.get(j + 1).score) {
+                    Collections.swap(allUsers, j, j + 1);
+                }
+                if (allUsers.get(j).score == allUsers.get(j + 1).score) {
+                    if (allUsers.get(j).getNickName().compareTo(allUsers.get(j + 1).getNickName()) > 0) {
+                        Collections.swap(allUsers, j, j + 1);
+                    }
+                }
+            }
+        }
     }
 
     @Override
--- src/main/java/View/Menu/ScoreBoard.java
+++ src/main/java/View/Menu/ScoreBoard.java
@@ -1,7 +1,53 @@
 package View.Menu;
 
+import Controller.Regex;
+import Model.User;
+
+import java.util.regex.Matcher;
+
 public class ScoreBoard extends Menu{
     public ScoreBoard(Menu parentMenu) {
         super("ScoreBoard Menu", parentMenu);
+    }
+
+    @Override
+    public void execute() {
+        String input = scanner.nextLine();
+        input = editSpaces(input);
+        if (Regex.getMatcher(input, Regex.showCurrentMenu).find()) {
+            this.showName();
+            this.execute();
+        } else if (Regex.getMatcher(input, Regex.menuExit).find()) {
+            this.menuExit();
+        } else if (Regex.getMatcher(input, Regex.menuEnter).find()) {
+            Matcher matcher = Regex.getMatcher(input, Regex.menuEnter);
+            if (matcher.find()) {
+                this.menuEnter(matcher.group(1));
+            }
+        } else if (Regex.getMatcher(input, Regex.userLogout).find()) {
+            this.logoutUser();
+        } else if (Regex.getMatcher(input, Regex.showScoreBoard).find()) {
+            showScoreBoard();
+            this.execute();
+        } else {
+            System.out.println("invalid command!");
+            this.execute();
+        }
+    }
+
+    private void showScoreBoard() {
+        int rank = 1;
+        System.out.println(rank + "- " + User.getAllUsers().get(0));
+        for (int i = 1; i < User.getAllUsers().size(); i++) {
+            if (User.getAllUsers().get(i).getScore() < User.getAllUsers().get(i - 1).getScore()) {
+                System.out.println(++rank + "- " + User.getAllUsers().get(i));
+            } else {
+                System.out.println(rank + "- " + User.getAllUsers().get(i));
+            }
+        }
+    }
+
+    private String editSpaces(String string) {
+        return string.replaceAll("(\\s)+", " ");
     }
 }
