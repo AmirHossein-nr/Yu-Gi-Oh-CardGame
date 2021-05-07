f92abe4302ae2ea12104569fa6d384c8e30d4f91
AmirHossein_Razlighi <arazlighi@gmail.com>
2021-05-07 23:26:19 +0430

    Added Model Board

---
+++ src/main/java/Model/Board.java
@@ -0,0 +1,58 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Board {
+
+    private Deck deck;
+    private Card fieldZone;
+    private ArrayList<Card> allCards;
+    private ArrayList<Card> monstersZone;
+    private ArrayList<Card> spellsAndTrapsZone;
+    private ArrayList<Card> graveYard;
+    private ArrayList<Card> cardsInHand;
+
+    public Board() {
+        allCards = new ArrayList<>();
+        monstersZone = new ArrayList<>();
+        spellsAndTrapsZone = new ArrayList<>();
+        graveYard = new ArrayList<>();
+        cardsInHand = new ArrayList<>();
+    }
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
+
+    public void setFieldZone(Card fieldZone) {
+        this.fieldZone = fieldZone;
+    }
+
+    public ArrayList<Card> getAllCards() {
+        return allCards;
+    }
+
+    public ArrayList<Card> getCardsInHand() {
+        return cardsInHand;
+    }
+
+    public ArrayList<Card> getGraveYard() {
+        return graveYard;
+    }
+
+    public ArrayList<Card> getMonstersZone() {
+        return monstersZone;
+    }
+
+    public ArrayList<Card> getSpellsAndTrapsZone() {
+        return spellsAndTrapsZone;
+    }
+
+    public Card getFieldZone() {
+        return fieldZone;
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
+}
