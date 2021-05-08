594918944be24a34b026bd1694f7dc020da7042b
Mostafa <mostafa.hosseini788@gmail.com>
2021-05-08 05:57:54 -0700

    Sorting cards alphabetically in Main and Side deck

--- src/main/java/Model/MainDeck.java
+++ src/main/java/Model/MainDeck.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainDeck {
     private ArrayList<Card> cardsInMainDeck;
@@ -15,24 +16,23 @@ public class MainDeck {
     //todo COMPLETE HERE !
 
     public String printMonsters() {
-
+        sortCardsInMainDeck();
         for (Card card : cardsInMainDeck) {
             if (card instanceof Monster) {
                 System.out.println(card.getName() + ": " + card.getDescription());
             }
         }
-        //todo Alphabetically
+
         return "";
     }
 
     public String printSpellsTraps() {
-
+        sortCardsInMainDeck();
         for (Card card : cardsInMainDeck) {
             if (card instanceof Spell || card instanceof Trap) {
                 System.out.println(card.getName() + ": " + card.getDescription());
             }
         }
-        //todo Alphabetically
 
         return "";
     }
@@ -63,8 +63,19 @@ public class MainDeck {
         return isValid;
     }
 
+    public void sortCardsInMainDeck() {
+        for (int i = cardsInMainDeck.size() - 2; i >= 0; i--) {
+            for (int j = 0; j <= cardsInMainDeck.size() - 2; j++) {
+                if (cardsInMainDeck.get(j).getName().compareTo(cardsInMainDeck.get(j + 1).getName()) > 0) {
+                    Collections.swap(cardsInMainDeck, j, j + 1);
+                }
+            }
+        }
+    }
+
     @Override
     public String toString() {
+
         return "Main Deck :\n" +
                 printMonsters() + "\nSpells and Traps :\n" +
                 printSpellsTraps();
--- src/main/java/Model/SideDeck.java
+++ src/main/java/Model/SideDeck.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class SideDeck {
     private ArrayList<Card> cardsInSideDeck;
@@ -11,24 +12,22 @@ public class SideDeck {
     }
 
     public String printMonsters() {
-
+        sortCardsInSideDeck();
         for (Card card : cardsInSideDeck) {
             if (card instanceof Monster) {
                 System.out.println(card.getName() + ": " + card.getDescription());
             }
         }
-        //todo Alphabetically
         return "";
     }
 
     public String printSpellsTraps() {
-
+        sortCardsInSideDeck();
         for (Card card : cardsInSideDeck) {
             if (card instanceof Spell || card instanceof Trap) {
                 System.out.println(card.getName() + ": " + card.getDescription());
             }
         }
-        //todo Alphabetically
 
         return "";
     }
@@ -49,6 +48,16 @@ public class SideDeck {
 
     public ArrayList<Card> getCardsInSideDeck() {
         return cardsInSideDeck;
+    }
+
+    public void sortCardsInSideDeck() {
+        for (int i = cardsInSideDeck.size() - 2; i >= 0; i--) {
+            for (int j = 0; j <= cardsInSideDeck.size() - 2; j++) {
+                if (cardsInSideDeck.get(j).getName().compareTo(cardsInSideDeck.get(j + 1).getName()) > 0) {
+                    Collections.swap(cardsInSideDeck, j, j + 1);
+                }
+            }
+        }
     }
 
     @Override
