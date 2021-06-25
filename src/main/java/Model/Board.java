daae284062fd0244a873d7920386e9bb8633b76c
Mostafa <mostafa.hosseini788@gmail.com>
2021-06-25 11:02:30 -0700

    added Herald of creation

--- src/main/java/Model/Board.java
+++ src/main/java/Model/Board.java
@@ -16,6 +16,7 @@ public class Board {
     private ArrayList<Card> cardsInHand;
     private HashMap<Card, Card> spellMonsterEquip = new HashMap<>();
     private HashMap<Card, ArrayList<Card>> commandKnights = new HashMap<>();
+    private ArrayList<Card> activatedHeraldOfCreation = new ArrayList<>();
 
     public Board() {
 
@@ -52,6 +53,14 @@ public class Board {
 
     public void setSpellMonsterEquip(HashMap<Card, Card> spellMonsterEquip) {
         this.spellMonsterEquip = spellMonsterEquip;
+    }
+
+    public void setActivatedHeraldOfCreation(ArrayList<Card> activatedHeraldOfCreation) {
+        this.activatedHeraldOfCreation = activatedHeraldOfCreation;
+    }
+
+    public ArrayList<Card> getActivatedHeraldOfCreation() {
+        return activatedHeraldOfCreation;
     }
 
     public HashMap<Card, ArrayList<Card>> getCommandKnights() {
