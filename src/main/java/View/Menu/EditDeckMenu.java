f6c2f532b3711452daae4ea4017afef8aa40b188
AmirHossein_Razlighi <arazlighi@gmail.com>
2021-07-08 21:00:07 +0430

    - editDeckMenu Complete

--- src/main/java/View/Menu/EditDeckMenu.java
+++ src/main/java/View/Menu/EditDeckMenu.java
@@ -2,6 +2,9 @@ package View.Menu;
 
 import Model.*;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.Effect;
 import javafx.scene.effect.Shadow;
@@ -10,9 +13,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 
-import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -29,6 +29,7 @@ public class EditDeckMenu extends Menu {
     public FlowPane sideDeck;
 
     private ArrayList<Card> cards = new ArrayList<>();
+    private ArrayList<ImageView> imageViews = new ArrayList<>();
 
     public EditDeckMenu() {
         super("Edit Deck", null);
@@ -36,6 +37,9 @@ public class EditDeckMenu extends Menu {
         deck = new Deck(new MainDeck(false), new SideDeck(false));
         loggedUser = new User("amirHossein", "1234", "amir");
         new Shop(null);
+        loggedUser.getAllCards().add(Shop.getAllCards().get(10));
+        loggedUser.getAllCards().add(Shop.getAllCards().get(10));
+        loggedUser.getAllCards().add(Shop.getAllCards().get(10));
         loggedUser.getAllCards().add(Shop.getAllCards().get(10));
         loggedUser.getAllCards().add(Shop.getAllCards().get(15));
         loggedUser.getAllCards().add(Shop.getAllCards().get(14));
@@ -67,14 +71,14 @@ public class EditDeckMenu extends Menu {
         allCardsPane.setHgap(10);
         allCardsPane.setVgap(10);
         for (Card card : loggedUser.getAllCards()) {
-            if (cards.contains(card)) continue;
             ImageView imageView = new ImageView();
             imageView.setFitWidth(100);
             imageView.setFitHeight(170);
             imageView.setImage(card.getCardImage());
             dragControl(imageView);
-            allCardsPane.getChildren().add(imageView);
+            imageViews.add(imageView);
         }
+        updateCards();
     }
 
     @FXML
@@ -82,15 +86,29 @@ public class EditDeckMenu extends Menu {
         mainDeck.setVgap(10);
         mainDeck.setHgap(10);
         for (Card card : cards) {
+            if (deck.getMainDeck().getCardsInMainDeck().size() >= 60) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText("ERROR");
+                alert.setContentText("Main Deck Is Full !");
+                alert.show();
+                return;
+            }
+            if (checkCards(card)) return;
             deck.getMainDeck().getCardsInMainDeck().add(card);
-            ImageView imageView = new ImageView();
-            imageView.setFitWidth(100);
-            imageView.setFitHeight(170);
-            imageView.setImage(card.getCardImage());
-            mainDeck.getChildren().add(imageView);
+            addImageView(card, mainDeck);
         }
         cards.clear();
-        showAllCards();
+        updateCards();
+    }
+
+    public void updateCards() {
+        allCardsPane.getChildren().removeIf(node -> !(node instanceof Button));
+        for (ImageView imageView : imageViews) {
+            imageView.setFitWidth(100);
+            imageView.setFitHeight(170);
+            dragControl(imageView);
+            allCardsPane.getChildren().add(imageView);
+        }
     }
 
     private void dragControl(ImageView imageView) {
@@ -111,14 +129,57 @@ public class EditDeckMenu extends Menu {
         sideDeck.setHgap(10);
 
         for (Card card : cards) {
+            if (deck.getSideDeck().getCardsInSideDeck().size() >= 15) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText("ERROR");
+                alert.setContentText("Side Deck Is Full !");
+                alert.show();
+                return;
+            }
+            if (checkCards(card)) return;
             deck.getSideDeck().getCardsInSideDeck().add(card);
-            ImageView imageView = new ImageView();
-            imageView.setFitWidth(100);
-            imageView.setFitHeight(170);
-            imageView.setImage(card.getCardImage());
-            sideDeck.getChildren().add(imageView);
+            addImageView(card, sideDeck);
         }
         cards.clear();
-        showAllCards();
+        updateCards();
+    }
+
+    private void addImageView(Card card, FlowPane sideDeck) {
+        ImageView imageView = new ImageView();
+        imageView.setFitWidth(100);
+        imageView.setFitHeight(170);
+        imageView.setImage(card.getCardImage());
+        for (ImageView view : imageViews) {
+            if (view.getImage() == imageView.getImage()) {
+                imageViews.remove(view);
+                break;
+            }
+        }
+        sideDeck.getChildren().add(imageView);
+    }
+
+    private boolean checkCards(Card card) {
+        int counter = 0;
+        for (Card myCard : deck.getMainDeck().getCardsInMainDeck()) {
+            if (myCard.getName().equals(card.getName()))
+                counter++;
+            if (counter >= 3)
+                break;
+        }
+        for (Card myCard : deck.getSideDeck().getCardsInSideDeck()) {
+            if (myCard.getName().equals(card.getName()))
+                counter++;
+            if (counter >= 3)
+                break;
+        }
+
+        if (counter >= 3) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Error!");
+            alert.setContentText("There Are Already Three Instances Of This Card In this Deck !");
+            alert.show();
+            return true;
+        }
+        return false;
     }
 }
