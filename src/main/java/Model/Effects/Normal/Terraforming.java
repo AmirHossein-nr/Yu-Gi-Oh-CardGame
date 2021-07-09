bb5f0f2f8170395b34b6a2a8dd4ddaf0c6262c81
Mostafa <mostafa.hosseini788@gmail.com>
2021-07-09 07:23:07 -0700

    terraforming effect added

--- src/main/java/Model/Effects/Normal/Terraforming.java
+++ src/main/java/Model/Effects/Normal/Terraforming.java
@@ -1,12 +1,31 @@
 package Model.Effects.Normal;
 
-import Model.Card;
+import Model.*;
 import Model.Effects.Effect;
-import Model.Spell;
-import Model.Type;
 import Controller.Game;
+import View.GUI.GamePlay;
+import animatefx.animation.FadeIn;
+import animatefx.animation.FadeOutUp;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.ArrayList;
 
 public class Terraforming extends Effect {
+
+    private CardRectangle selectedFieldSpellRectangle = null;
 
     public Terraforming(Card card) {
         super(card);
@@ -15,33 +34,69 @@ public class Terraforming extends Effect {
 
     @Override
     public boolean activate(Game game) {
-        if (canBeActivated(game)) {
-            System.out.println("enter number of field spell in your deck");
-            while (true) {
-                String answer = editSpaces(scanner.nextLine());
-                if (answer.equals("cancel")) {
-                    System.out.println("canceled");
-                    return false;
-                } else if (!answer.matches("\\d+")) {
-                    System.out.println("enter a number");
-                } else {
-                    int number = Integer.parseInt(answer);
-                    Card card = game.getCurrentUser().getBoard().getDeckZone().get(number - 1);
-                    if (card instanceof Spell) {
-                        if (card.getCardType() == Type.FIELD) {
-                            game.getCurrentUser().getBoard().addCardFromDeckToHand(number - 1);
-                            System.out.println("spell activated");
-                            game.addSpellOrTrapFromZoneToGraveyard(card, game.getCurrentUser());
-                            return true;
-                        }
-                    }
-                    System.out.println("select a field spell");
+        HBox box = new HBox(50);
+        box.setAlignment(Pos.TOP_LEFT);
+        box.setPadding(new Insets(10));
+        GridPane gridPane = new GridPane();
+        gridPane.setLayoutX(0);
+        gridPane.setLayoutY(0);
+        box.getChildren().add(gridPane);
+
+        ArrayList<CardRectangle> fieldSpellRectangles = new ArrayList<>();
+
+        for (int i = 0; i < game.getCurrentUser().getBoard().getGraveYard().size(); i++) {
+            if (game.getCurrentUser().getBoard().getGraveYard().get(i).getCardType() == Type.FIELD) {
+                CardRectangle rectangle = new CardRectangle();
+                rectangle.setWidth(90);
+                rectangle.setHeight(150);
+                rectangle.setFill(new ImagePattern(game.getCurrentUser().getBoard().getGraveYard().get(i).getCardImage()));
+                rectangle.setRelatedCard(game.getCurrentUser().getBoard().getGraveYard().get(i));
+                rectangle.setOnMouseClicked(event1 -> {
+                    if (selectedFieldSpellRectangle != null) selectedFieldSpellRectangle.setStroke(Color.TRANSPARENT);
+                    selectedFieldSpellRectangle = rectangle;
+                    selectedFieldSpellRectangle.setStroke(Color.GOLD);
+                });
+                fieldSpellRectangles.add(rectangle);
+            }
+        }
+        int z = 0;
+        outer:
+        for (int i = 0; ; i++) {
+            for (int j = 0; j < 10; j++) {
+                try {
+                    gridPane.add(fieldSpellRectangles.get(z), j, i);
+                    z++;
+                } catch (Exception e) {
+                    break outer;
                 }
             }
-        } else {
-            System.out.println("preparations of this spell are not done yet");
-            return false;
         }
+
+        Button add = new Button("add");
+        Stage popupStage = new Stage(StageStyle.TRANSPARENT);
+        popupStage.initOwner(Game.mainStage);
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        Scene question = new Scene(box, Color.TRANSPARENT);
+        question.getStylesheets().add("/Css/GamePlay.css");
+        popupStage.setScene(question);
+        add.setOnMouseClicked(event -> {
+            if (selectedFieldSpellRectangle == null) {
+                GamePlay.showAlert(Alert.AlertType.ERROR, "Error !",
+                        "No Card is Selected!");
+            } else {
+                new FadeOutUp(box).play();
+                popupStage.hide();
+                Card fieldSpell = selectedFieldSpellRectangle.getRelatedCard();
+                game.getCurrentUser().getBoard().getDeckZone().remove(fieldSpell);
+                game.getCurrentUser().getBoard().getCardsInHand().add(fieldSpell);
+                GamePlay.showAlert(Alert.AlertType.INFORMATION, "activate effect message", "spell activated");
+                game.addSpellOrTrapFromZoneToGraveyard(card, game.getCurrentUser());
+            }
+        });
+        box.getChildren().add(add);
+        popupStage.show();
+        new FadeIn(box).play();
+        return true;
     }
 
     @Override
